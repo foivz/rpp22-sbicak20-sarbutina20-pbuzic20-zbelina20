b946316b6d4b6b6957124c35eac67c8d02dea030
pbuzic20 <pbuzic20@student.foi.hr>
2022-12-03 19:49:25 +0100

    Implementirana forma za dodavanje i uređivanje klijenta

---
+++ Software/ZMGDesktop/ZMGDesktop/FrmDodajKlijenta.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace ZMGDesktop
+{
+    partial class FrmDodajKlijenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNaziv = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOIB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAdresa = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtIBAN = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMjesto = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.btnNatrag = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(100, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dodaj klijenta";
+            // 
+            // txtNaziv
+            // 
+            this.txtNaziv.Location = new System.Drawing.Point(64, 111);
+            this.txtNaziv.Name = "txtNaziv";
+            this.txtNaziv.Size = new System.Drawing.Size(215, 22);
+            this.txtNaziv.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Naziv:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "OIB:";
+            // 
+            // txtOIB
+            // 
+            this.txtOIB.Location = new System.Drawing.Point(64, 170);
+            this.txtOIB.Name = "txtOIB";
+            this.txtOIB.Size = new System.Drawing.Size(215, 22);
+            this.txtOIB.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Adresa:";
+            // 
+            // txtAdresa
+            // 
+            this.txtAdresa.Location = new System.Drawing.Point(64, 228);
+            this.txtAdresa.Name = "txtAdresa";
+            this.txtAdresa.Size = new System.Drawing.Size(215, 22);
+            this.txtAdresa.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 265);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "IBAN:";
+            // 
+            // txtIBAN
+            // 
+            this.txtIBAN.Location = new System.Drawing.Point(64, 284);
+            this.txtIBAN.Name = "txtIBAN";
+            this.txtIBAN.Size = new System.Drawing.Size(215, 22);
+            this.txtIBAN.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(61, 322);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Mjesto:";
+            // 
+            // txtMjesto
+            // 
+            this.txtMjesto.Location = new System.Drawing.Point(64, 341);
+            this.txtMjesto.Name = "txtMjesto";
+            this.txtMjesto.Size = new System.Drawing.Size(215, 22);
+            this.txtMjesto.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(61, 376);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Broj telefona:";
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(64, 395);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(215, 22);
+            this.txtTelefon.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(61, 427);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 16);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(64, 446);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(215, 22);
+            this.txtEmail.TabIndex = 13;
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(64, 503);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(82, 37);
+            this.btnSpremi.TabIndex = 15;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            // 
+            // btnNatrag
+            // 
+            this.btnNatrag.Location = new System.Drawing.Point(167, 503);
+            this.btnNatrag.Name = "btnNatrag";
+            this.btnNatrag.Size = new System.Drawing.Size(90, 37);
+            this.btnNatrag.TabIndex = 16;
+            this.btnNatrag.Text = "Natrag";
+            this.btnNatrag.UseVisualStyleBackColor = true;
+            // 
+            // FrmDodajKlijenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 552);
+            this.Controls.Add(this.btnNatrag);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMjesto);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtIBAN);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAdresa);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOIB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNaziv);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmDodajKlijenta";
+            this.Text = "Dodaj klijenta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNaziv;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOIB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAdresa;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtIBAN;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMjesto;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.Button btnNatrag;
+    }
+}
