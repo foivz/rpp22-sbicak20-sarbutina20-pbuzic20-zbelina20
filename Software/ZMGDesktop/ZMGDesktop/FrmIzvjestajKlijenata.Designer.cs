38e0cc3942f27d72ff6c6ac26d6d29f5fd36f3b7
pbuzic20 <pbuzic20@student.foi.hr>
2022-12-03 20:28:02 +0100

    Implementirana forma za prikaz svih izvještaja i izvještaja 10 najvećih klijenata

---
+++ Software/ZMGDesktop/ZMGDesktop/FrmIzvjestajKlijenata.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ZMGDesktop
+{
+    partial class FrmIzvjestajKlijenata
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dgvIzvjestajKlijenata = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.btnNatrag = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIzvjestajKlijenata)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvIzvjestajKlijenata
+            // 
+            this.dgvIzvjestajKlijenata.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvIzvjestajKlijenata.Location = new System.Drawing.Point(12, 67);
+            this.dgvIzvjestajKlijenata.Name = "dgvIzvjestajKlijenata";
+            this.dgvIzvjestajKlijenata.RowHeadersWidth = 51;
+            this.dgvIzvjestajKlijenata.RowTemplate.Height = 24;
+            this.dgvIzvjestajKlijenata.Size = new System.Drawing.Size(513, 371);
+            this.dgvIzvjestajKlijenata.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(349, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Izvještaj 10 najvećih klijenata";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(583, 93);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Legend1";
+            series1.Name = "Klijent1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(696, 413);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(108, 35);
+            this.btnSpremi.TabIndex = 3;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            // 
+            // btnNatrag
+            // 
+            this.btnNatrag.Location = new System.Drawing.Point(815, 413);
+            this.btnNatrag.Name = "btnNatrag";
+            this.btnNatrag.Size = new System.Drawing.Size(108, 35);
+            this.btnNatrag.TabIndex = 4;
+            this.btnNatrag.Text = "Natrag";
+            this.btnNatrag.UseVisualStyleBackColor = true;
+            // 
+            // FrmIzvjestajKlijenata
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 453);
+            this.Controls.Add(this.btnNatrag);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvIzvjestajKlijenata);
+            this.Name = "FrmIzvjestajKlijenata";
+            this.Text = "Izvještaj klijenata";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIzvjestajKlijenata)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvIzvjestajKlijenata;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.Button btnNatrag;
+    }
+}
