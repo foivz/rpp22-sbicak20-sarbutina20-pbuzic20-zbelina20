daa8b2da58572a7c6b79393368e98bd129537e12
pbuzic20 <pbuzic20@student.foi.hr>
2022-12-04 22:34:37 +0100

    Implementirana forma za glavni izbornik

---
+++ Software/ZMGDesktop/ZMGDesktop/FrmPocetna.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ZMGDesktop
+{
+    partial class FrmPocetna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKlijenti = new System.Windows.Forms.Button();
+            this.btnRacuni = new System.Windows.Forms.Button();
+            this.btnStanjeSkladista = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnPodrska = new System.Windows.Forms.Button();
+            this.btnRadniNalozi = new System.Windows.Forms.Button();
+            this.btnIzvjestaji = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnKlijenti
+            // 
+            this.btnKlijenti.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnKlijenti.Location = new System.Drawing.Point(47, 101);
+            this.btnKlijenti.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnKlijenti.Name = "btnKlijenti";
+            this.btnKlijenti.Size = new System.Drawing.Size(149, 46);
+            this.btnKlijenti.TabIndex = 13;
+            this.btnKlijenti.Text = "Klijenti";
+            this.btnKlijenti.UseVisualStyleBackColor = true;
+            // 
+            // btnRacuni
+            // 
+            this.btnRacuni.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnRacuni.Location = new System.Drawing.Point(47, 185);
+            this.btnRacuni.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRacuni.Name = "btnRacuni";
+            this.btnRacuni.Size = new System.Drawing.Size(149, 46);
+            this.btnRacuni.TabIndex = 14;
+            this.btnRacuni.Text = "Računi";
+            this.btnRacuni.UseVisualStyleBackColor = true;
+            // 
+            // btnStanjeSkladista
+            // 
+            this.btnStanjeSkladista.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStanjeSkladista.Location = new System.Drawing.Point(47, 265);
+            this.btnStanjeSkladista.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnStanjeSkladista.Name = "btnStanjeSkladista";
+            this.btnStanjeSkladista.Size = new System.Drawing.Size(149, 46);
+            this.btnStanjeSkladista.TabIndex = 15;
+            this.btnStanjeSkladista.Text = "Stanje skladišta";
+            this.btnStanjeSkladista.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ZMGDesktop.Properties.Resources.ZMG;
+            this.pictureBox1.Location = new System.Drawing.Point(238, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(239, 214);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnPodrska
+            // 
+            this.btnPodrska.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnPodrska.Location = new System.Drawing.Point(517, 265);
+            this.btnPodrska.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPodrska.Name = "btnPodrska";
+            this.btnPodrska.Size = new System.Drawing.Size(149, 46);
+            this.btnPodrska.TabIndex = 19;
+            this.btnPodrska.Text = "Podrška";
+            this.btnPodrska.UseVisualStyleBackColor = true;
+            // 
+            // btnRadniNalozi
+            // 
+            this.btnRadniNalozi.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnRadniNalozi.Location = new System.Drawing.Point(517, 185);
+            this.btnRadniNalozi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRadniNalozi.Name = "btnRadniNalozi";
+            this.btnRadniNalozi.Size = new System.Drawing.Size(149, 46);
+            this.btnRadniNalozi.TabIndex = 18;
+            this.btnRadniNalozi.Text = "Radni nalozi";
+            this.btnRadniNalozi.UseVisualStyleBackColor = true;
+            // 
+            // btnIzvjestaji
+            // 
+            this.btnIzvjestaji.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnIzvjestaji.Location = new System.Drawing.Point(517, 101);
+            this.btnIzvjestaji.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnIzvjestaji.Name = "btnIzvjestaji";
+            this.btnIzvjestaji.Size = new System.Drawing.Size(149, 46);
+            this.btnIzvjestaji.TabIndex = 17;
+            this.btnIzvjestaji.Text = "Izvještaji";
+            this.btnIzvjestaji.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 39);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Dobrodošli!";
+            // 
+            // FrmPocetna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 368);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPodrska);
+            this.Controls.Add(this.btnRadniNalozi);
+            this.Controls.Add(this.btnIzvjestaji);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStanjeSkladista);
+            this.Controls.Add(this.btnRacuni);
+            this.Controls.Add(this.btnKlijenti);
+            this.Name = "FrmPocetna";
+            this.Text = "Glavni izbornik";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKlijenti;
+        private System.Windows.Forms.Button btnRacuni;
+        private System.Windows.Forms.Button btnStanjeSkladista;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnPodrska;
+        private System.Windows.Forms.Button btnRadniNalozi;
+        private System.Windows.Forms.Button btnIzvjestaji;
+        private System.Windows.Forms.Label label1;
+    }
+}
