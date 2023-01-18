6016e228830e59c5b884a7222a12b8e889f5ca47
okasus <okasus@DESKTOP-ISPNP4F>
2023-01-19 00:04:41 +0100

    Merge za sve forme za račune.

---
+++ Software/ZMGDesktop/ZMGDesktop/FrmPregledRacunaPDF.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZMGDesktop
+{
+    public partial class FrmPregledRacunaPDF : Form
+    {
+        public FrmPregledRacunaPDF()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPregledRacunaPDF_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
