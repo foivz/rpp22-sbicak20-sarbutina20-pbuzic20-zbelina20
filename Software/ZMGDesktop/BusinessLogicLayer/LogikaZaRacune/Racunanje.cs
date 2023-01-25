90e0f9877ecdeef750587b4681a91790507df477
sbicak20 <sbicak20@student.foi.hr>
2023-01-25 22:46:48 +0100

    Dodana logika za racunanje iznosa na racunu s PDV-om.

---
+++ Software/ZMGDesktop/BusinessLogicLayer/LogikaZaRacune/Racunanje.cs
@@ -0,0 +1,36 @@
+﻿using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace BusinessLogicLayer.LogikaZaRacune
+{
+    public class Racunanje
+    {
+        public double RacunanjeUkupnog(List<StavkaRacun> stavke)
+        {
+            double ukupno = 0;
+
+            foreach(var st in stavke)
+            {
+                ukupno += st.UkupnaCijenaStavke;
+            }
+
+            return ukupno;
+        }
+
+        public double RacunanjePDV(double ukupno)
+        {
+            ukupno = ukupno * 0.25;
+            return ukupno;
+        }
+
+        public double RacunanjeUkupnogPDV(double iznosStavki, double ukupnoPDV)
+        {
+            return iznosStavki + ukupnoPDV;
+        }
+    }
+}
